550777c801750b3acd5ef7080a51dc2b26fd8284
Ankit98550 <ankit887gupta@gmail.com>
2024-01-17 09:27:27 +0530

    FirstDay

---
+++ Python/first_day.py
@@ -0,0 +1,65 @@
+# WAP to check the number is greater or not
+"""a=int(input('enter the number'))
+b=input('enter the number')
+if (a>b):
+    p(rint('a is greater than b')
+elif(a==b):
+    print('a is equal to b')
+else:
+    print('a is less than b')"""
+
+# WAP to check the number is divisible by 3
+'''a=int(input('enter the number'))
+if(a%3==0):
+    print('is it divisibe')
+else:
+    print('not divisible')'''
+
+# WAP to check vowel is present or not
+"""vowel=('a', 'i', 'e', 'o', 'u')
+word=input(('enter the number '))
+
+if any(v in word for v in vowel):
+    print('vowel present')
+else:
+    print('not present')
+"""
+
+# WAP a program to convert negative integer to positive
+"""x = int(input("Enter a number : "))
+
+if x < 0:
+    print(x*-1)
+else:
+    print(x)"""
+
+# WAP to built a Calculator
+"""num1 = int(input("Enter a Number "))
+num2 = int(input("Enter a Number "))
+num3=input("Choose Operation ")
+if num3 =="+":
+    print(num1+num2)
+elif num3 =="-":
+    print(num1-num2)
+elif num3 =="*":
+    print(num1*num2)
+elif num3=="/":
+    print(num1/num2)
+else:
+    print("Not Defined")"""
+
+# WAP to check the character is vowel or not
+"""char = input("Enter a Character : ")
+if char =="a" :
+    print("Vowel is present")
+elif char =="i":
+    print("vowel is Present")
+elif char =="e":
+    print("vowel is Present")
+elif char =="o":
+    print("vowel is Present")
+elif char =="u":
+    print("vowel is Present")
+else:
+    print("Vowel is not present")"""
+
